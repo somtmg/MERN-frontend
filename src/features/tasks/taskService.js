01267e01dcf27f8e5f240cafb6a3e84e8454ecbc
somtmg <sombdrtmg1@gmail.com>
2025-08-26 16:08:58 +0545

    connected to backend

--- src/features/tasks/taskService.js
+++ src/features/tasks/taskService.js
@@ -1,34 +1,34 @@
-import axios from 'axios'
+import axios from "axios"
 
-const API_URL = '/api/tasks/'
+const API_URL = "https://mern-ultimate-backend.onrender.com/api/tasks/"
 
 const createTask = async (taskData, token) => {
-    const config = {
-        headers: {
-            Authorization: `Bearer ${token}`,
-        }
-    }
-    const response = await axios.post(API_URL, taskData, config)
+  const config = {
+    headers: {
+      Authorization: `Bearer ${token}`,
+    },
+  }
+  const response = await axios.post(API_URL, taskData, config)
 
-    return response.data
+  return response.data
 }
 const getTasks = async (token) => {
-    const config = {
-        headers: {
-            Authorization: `Bearer ${token}`,
-        }
-    }
-    const response = await axios.get(API_URL, config)
-    return response.data
+  const config = {
+    headers: {
+      Authorization: `Bearer ${token}`,
+    },
+  }
+  const response = await axios.get(API_URL, config)
+  return response.data
 }
 const deleteTask = async (id, token) => {
-    const config = {
-        headers: {
-            Authorization: `Bearer ${token}`,
-        }
-    }
-    const response = await axios.delete(API_URL + id, config)
-    return response.data
+  const config = {
+    headers: {
+      Authorization: `Bearer ${token}`,
+    },
+  }
+  const response = await axios.delete(API_URL + id, config)
+  return response.data
 }
 
 const taskService = { createTask, getTasks, deleteTask }
